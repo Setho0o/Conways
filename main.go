16ca48ee51f0598eab0dba8c8e9e8d0849850ad9
Setho0o <sethryderharris@gmail.com>
2025-02-18 18:12:47 -0600

    took longer than I expected

--- main.go
+++ main.go
@@ -1,66 +1,82 @@
 package main
 
 import (
-	"fmt"
 	"image/color"
 	"log"
+	"math/rand"
 	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 type Mode int
+type Direction int
 
 const (
-  blocksize int = 10
-  LayoutX int = ScreenX
-  LayoutY int = ScreenY
-  ScreenX int = 960
-  ScreenY int = 540
-Play Mode = iota
-  Pause  
-  Restart 
+	randamt   int  = 20
+	blocksize int  = 6
+	LayoutX   int  = ScreenX
+	LayoutY   int  = ScreenY
+	ScreenX   int  = 1920
+	ScreenY   int  = 1080
+	Play      Mode = iota
+	Pause
+	TopLeft Direction = iota
+	TopMiddle
+	TopRight
+	Left
+	Right
+	BottomLeft
+	BottomMiddle
+	BottomRight
 )
 
+var Dir = []Direction{
+	TopLeft, TopMiddle, TopRight, Left, Right, BottomLeft, BottomMiddle, BottomRight,
+}
+
 type Cell struct {
-  alive bool 
-  x int
-  y int
-  bx []int //bounds of the cell 
-  by []int
+	alive bool
+	x     int
+	y     int
+	ix    int //index
+	iy    int
 }
 
-func (c *Cell) Bounds() {
-	for x := c.x; x < c.x+blocksize; x++ {
-		c.bx = append(c.bx, x)
-	}
-	for y := c.y; y < c.y+blocksize; y++ {
-		c.by = append(c.by, y)
-	}
-}
-
-func (g *Game) FillBounds(c Cell) {
-	for _, x := range c.bx {
-		for _, y := range c.by {
-			g.s.Set(x, y, color.White)
+func (g *Game) Fill(c Cell, color color.Color) {
+	for i := range blocksize {
+		for j := range blocksize {
+			g.s.Set(c.x+i, c.y+j, color)
 		}
 	}
 }
 
-type Game struct{
-  m Mode
-  matrix [][]Cell
-  s *ebiten.Image
+type Game struct {
+	m      Mode
+	matrix [][]*Cell
+	s      *ebiten.Image
 }
 
 func (g *Game) Update() error {
-  g.Keys()
+	g.Keys()
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-  g.s = screen
-  g.Life()
+	g.s = screen
+	if g.m == Play {
+		g.Life()
+	} else {
+		for _, e := range g.matrix {
+			for _, cell := range e {
+				if cell.alive {
+					g.Fill(*cell, color.White)
+				} else {
+					g.Fill(*cell, color.Black)
+				}
+			}
+		}
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -68,64 +84,143 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
-	ebiten.SetWindowSize(ScreenX,ScreenY)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(GameInit()); err != nil {
+	ebiten.SetWindowSize(ScreenX, ScreenY)
+	game := GameInit()
+	game.LifeRoulete()
+
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func GameInit() *Game {
-  return &Game {
-    m: Play,
-    matrix: MatrixInit(),
-  }
-}
-func MatrixInit() [][]Cell {
-  m := make([][]Cell, LayoutY / blocksize)
-  for i := range m {
-    m[i] = make([]Cell,LayoutX / blocksize)
-  }
-
-  for i, row := range m {
-		for j, _ := range row {
-			m[i][j] = Cell {
-        x: j * blocksize, 
-        y: i * blocksize, 
-        alive: false,
-      }
-      m[i][j].Bounds()
-    }
+	return &Game{
+		m:      Play,
+		matrix: MatrixInit(),
 	}
-  return m
 }
+
+func MatrixInit() [][]*Cell {
+	m := make([][]*Cell, LayoutY/blocksize)
+	for i := range m {
+		m[i] = make([]*Cell, LayoutX/blocksize)
+	}
+
+	for i, row := range m {
+		for j, _ := range row {
+			m[i][j] = &Cell{
+				x:     j * blocksize,
+				y:     i * blocksize,
+				ix:    j,
+				iy:    i,
+				alive: false,
+			}
+		}
+	}
+	return m
+}
+
+func (g *Game) LifeRoulete() {
+	for _, e := range g.matrix {
+		for _, cell := range e {
+			if rand.Intn(randamt) == 0 {
+				cell.alive = true
+			}
+		}
+	}
+}
+
 func (g *Game) Keys() {
-  if ebiten.IsKeyPressed(ebiten.KeyQ) {
-    os.Exit(0)
-  } else if ebiten.IsKeyPressed(ebiten.KeyA) {
-    if g.m == Play {
-      g.m = Pause
-    }
-  } else if ebiten.IsKeyPressed(ebiten.KeyS) {
-    if g.m == Pause {
-      g.m = Play
-    }
-  } else if ebiten.IsKeyPressed(ebiten.KeyD) {
-    g.m = Restart
-    fmt.Println("restart")
-  }
+	if ebiten.IsKeyPressed(ebiten.KeyQ) {
+		os.Exit(0)
+	} else if ebiten.IsKeyPressed(ebiten.KeyA) {
+		if g.m == Play {
+			g.m = Pause
+		}
+	} else if ebiten.IsKeyPressed(ebiten.KeyS) {
+		if g.m == Pause {
+			g.m = Play
+		}
+	} else if ebiten.IsKeyPressed(ebiten.KeyD) {
+		for _, e := range g.matrix {
+			for _, cell := range e {
+				cell.alive = false
+			}
+		}
+		g.LifeRoulete()
+	}
 }
 func (g *Game) Life() {
-  for _, e := range g.matrix {
-    for _, cell := range e {
-      g.s.Set(cell.x,cell.y, color.White)
-    }
-  }
+	for _, e := range g.matrix {
+		for _, cell := range e {
+			if OutOfBounds(*cell) {
+				continue
+			}
+			g.ConwaysRules(*cell)
+			if cell.alive {
+				g.Fill(*cell, color.White)
+			} else {
+				g.Fill(*cell, color.Black)
+			}
+		}
+	}
 }
 
+func (g *Game) ConwaysRules(c Cell) {
+	var life int
+	for _, e := range Dir {
+		if g.GetCellLife(c, e) {
+			life++
+		}
+	}
+	if c.alive {
+		if life < 2 { // rule 1: any cell with fewer than two alive neighbors dies
+			g.matrix[c.iy][c.ix].alive = false
+		} else if life == 2 || life == 3 { // rule 2: any cell with two or three neighbors lives on
+			g.matrix[c.iy][c.ix].alive = true
+		} else if life > 3 { // rule 3: any cell with more than three live neighbors dies
+			g.matrix[c.iy][c.ix].alive = false
+		}
+	} else if life == 3 { // rule 4: any dead cell with exactly three live neighbors becomes a live cell
+		g.matrix[c.iy][c.ix].alive = true
+	}
+}
 
+func (g *Game) GetCellLife(c Cell, d Direction) bool {
+	var life bool
+	switch d {
+	case TopLeft:
+		life = g.matrix[c.iy-1][c.ix-1].alive
+	case TopMiddle:
+		life = g.matrix[c.iy-1][c.ix].alive
+	case TopRight:
+		life = g.matrix[c.iy-1][c.ix+1].alive
+	case Left:
+		life = g.matrix[c.iy][c.ix-1].alive
+	case Right:
+		life = g.matrix[c.iy][c.ix+1].alive
+	case BottomLeft:
+		life = g.matrix[c.iy+1][c.ix-1].alive
+	case BottomMiddle:
+		life = g.matrix[c.iy+1][c.ix].alive
+	case BottomRight:
+		life = g.matrix[c.iy+1][c.ix+1].alive
+	}
+	return life
+}
 
-
-
-
-
+func OutOfBounds(c Cell) bool {
+	if c.y < 1 || c.y > LayoutY {
+		return true
+	}
+	if c.x < 1 || c.x > LayoutX {
+		return true
+	}
+	if c.iy < 0 || c.iy > LayoutY/blocksize-2 {
+		return true
+	}
+	if c.ix < 0 || c.ix > LayoutX/blocksize-2 {
+		return true
+	}
+	return false
+}
